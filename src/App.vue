910cadbe723f248f24b9d7118def34ee5ffae56a
Robson Freitas <robson.flavio20@outlook.com>
2022-10-09 00:40:43 -0300

    Ajustando componentes dos CARROS

--- src/App.vue
+++ src/App.vue
@@ -5,44 +5,44 @@
 <script>
 import TabMenu from 'primevue/tabmenu'
 
-export default({
-  data(){
+export default ({
+  data() {
     return {
       items: [
-                {label: 'Home', icon: 'pi pi-fw pi-home', to: '/'},
-                {label: 'About', icon: 'pi pi-fw pi-calendar', to: '/about'},
-                {label: 'Marcas', icon: 'pi pi-fw pi-pencil', to:'/Marca'},
-                {label: 'Carros', icon: 'pi pi-fw pi-file', to:'/Carro'},
-                {label: 'Cores', icon: 'pi pi-fw pi-cog', to:'/Cor'}
-            ]
-  }
-},
-components: {
-    TabMenu 
+        { label: 'Home', icon: 'pi pi-fw pi-home', to: '/' },
+        { label: 'About', icon: 'pi pi-fw pi-calendar', to: '/about' },
+        { label: 'Marcas', icon: 'pi pi-fw pi-pencil', to: '/Marca' },
+        { label: 'Carros', icon: 'pi pi-fw pi-file', to: '/Carro' },
+        { label: 'Cores', icon: 'pi pi-fw pi-cog', to: '/Cor' }
+      ]
+    }
+  },
+  components: {
+    TabMenu
   }
 })
 </script>
 
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-  
-  nav {
-    padding: 30px;
-  }
-  
-  nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-  
-  nav a.router-link-exact-active {
-    color: #42b983;
-  }
-  </style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- src/views/CarroView.vue
+++ src/views/CarroView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Carro</h1>
+        <h1 class="title-brand">Carros</h1>
 
         <table>
             <tr>
@@ -16,22 +16,58 @@
                 <td>{{ c.anoFabricacao }}</td>
                 <td>{{ c.anoModelo }}</td>
                 <td>{{ c.preco }}</td>
-                <td> <a href="javascript:void(0)" @click="editar(c)">Editar</a></td>
-                <td> <a href="javascript:void(0)" @click="excluir(c)">Excluir</a></td>
+                <td @click="carro = c">
+                <ConfirmDialog @click="messageDialog(c)"></ConfirmDialog>
+
+                <SplitButton label="Novo" icon="pi pi-plus" :model="items" href="javascript:void(0)">
+                </SplitButton>
+                </td>
 
             </tr>
         </table>
+        
+        <div class="col-lg-12" >
+            <button type="button" @click="novo()" class="btn btn-outline-primary">Nova marca</button>
+        </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
+import SplitButton from 'primevue/splitbutton'
+import ConfirmDialog from 'primevue/confirmdialog';
 
 export default {
     data() {
         return {
             //https://carros-app-example.herokuapp.com/carro
-            carros: []
+            carros: [],
+            carro:null,
+            items: [
+                {
+                    label: 'Editar',
+                    icon: 'pi pi-refresh',
+                    command: () => {
+                        this.editar(this.carro)
+                        this.$toast.add({ severity: 'success', summary: 'Updated', detail: 'Data Updated', life: 3000 });
+                    }
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-times',
+                    command: () => {
+                        this.messageDialog(this.carro)
+                        this.$toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000 });
+                    }
+                },
+                {
+                    label: 'Vue Website',
+                    icon: 'pi pi-external-link',
+                    command: () => {
+                        window.location.href = 'https://vuejs.org/'
+                    }
+                }
+            ]
         }
 
     },
@@ -57,8 +93,52 @@ export default {
             .then( resp => {
                 this.carros = resp.data
             })
+            window.location.reload();
+        },
+        messageDialog(car) {
+            this.$confirm.require({
+                message: `Você deseja deletar ${car.nome}?`,
+                header: 'Confirmar exclusão!!!',
+                icon: 'pi pi-exclamation-triangle',
+                acceptLabel:'Sim',
+                rejectLabel: 'Não',
+                accept: () => {
+                    this.excluir(car)
+                    this.$confirm.close();
+                    this.load()
+                },
+                reject: () => {
+                    this.load()
+                    this.$confirm.close()
+
+                },
+                onHide: () => {
+                    this.load()
+                    this.$confirm.close()
+                }
+            })
         }
+    },
+    components: {
+        SplitButton,
+        ConfirmDialog
     }
 
 }
 </script>
+
+<style>
+.col-lg-12 {
+    text-align: right;
+    padding: 10px;
+    margin-right: 20px;
+}
+.title-brand{
+    margin-top: 15px;
+    margin-bottom: 25px;
+
+    font-family: 'Bitter', serif;
+    font-size: 40px;
+    font-weight: bold;
+}
+</style>
